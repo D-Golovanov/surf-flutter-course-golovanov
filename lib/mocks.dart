36066bd7f10de9cc9e6aab71cce8339988d7118f
D-Dolovanov <da.golovanov@yandex.ru>
2023-03-16 23:49:35 +0300

    4-6-1-mock-data

---
+++ lib/mocks.dart
@@ -0,0 +1,32 @@
+import 'package:places/domain/sight.dart';
+
+final List<Sight> mocks = [
+  Sight(
+    name: 'Воронежский областной краеведческий музей',
+    lat: '78.198408',
+    lon: '48.5106548',
+    url:
+        'https://visit-voronezh.ru/assets/components/phpthumbof/cache/0bd7bd9d67a0c362eca6811d8cd1a1e1-w2048-h2048.196a527c526537c45ead89437634006a264.jpg',
+    details:
+        'Пряный вкус радостной жизни вместе с шеф-поваром Изо Дзандзава, благодаря которой у гостей ресторана есть возможность выбирать из двух направлений: европейского и восточного',
+    type: 'Музей',
+  ),
+  Sight(
+    name: 'Name 2',
+    lat: '78.198408',
+    lon: '48.5106548',
+    url:
+        'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/node/56828/mariya-shevchenko-kardiogramma-gor-462677-beluha.jpg?itok=X2o_utMw',
+    details: 'details details details details',
+    type: 'Ресторан',
+  ),
+  Sight(
+    name: 'Name 3',
+    lat: '78.198408',
+    lon: '48.5106548',
+    url:
+        'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/node/56828/mariya-shevchenko-kardiogramma-gor-462677-beluha.jpg?itok=X2o_utMw',
+    details: 'details details details details',
+    type: 'Музей',
+  ),
+];
