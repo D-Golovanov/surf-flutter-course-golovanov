d06b1aa83ff5140781d2bbbfafac57989ccabbf3
D-Dolovanov <da.golovanov@yandex.ru>
2023-03-19 13:37:30 +0300

    4-6-2-desing-screen-interesting-places

--- lib/assets/res.dart
+++ lib/assets/res.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:places/assets/colors.dart';
 
 class AppAssets {
-  static const favorite = 'res/icons/heart_icon.svg';
+  static const favorite = 'res/images/icons/heart.svg';
+}
+
+class AppDimensions {
+  static const k16 = 16.0;
 }
 
 class AppTypography {
@@ -11,5 +15,26 @@ class AppTypography {
     fontSize: 32.0,
     fontWeight: FontWeight.w700,
     color: AppColors.darkBlue,
+  );
+
+  static const textCardTitle = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16.0,
+    height: 1.25,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkTextBlue,
+  );
+  static const textCardDescription = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14.0,
+    height: 18.0 / 14.0,
+    color: AppColors.lightTextBlue,
+  );
+  static const textCardCategory = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14.0,
+    height: 18.0 / 14.0,
+    fontWeight: FontWeight.w700,
+    color: AppColors.white,
   );
 }
--- lib/mocks.dart
+++ lib/mocks.dart
@@ -7,17 +7,16 @@ final List<Sight> mocks = [
     lon: '48.5106548',
     url:
         'https://visit-voronezh.ru/assets/components/phpthumbof/cache/0bd7bd9d67a0c362eca6811d8cd1a1e1-w2048-h2048.196a527c526537c45ead89437634006a264.jpg',
-    details:
-        'Пряный вкус радостной жизни вместе с шеф-поваром Изо Дзандзава, благодаря которой у гостей ресторана есть возможность выбирать из двух направлений: европейского и восточного',
+    details: 'закрыто до 10:00',
     type: 'Музей',
   ),
   Sight(
-    name: 'Name 2',
+    name: 'Name 2 Name 2 Name 2 Name 2 Name 2 Name 2',
     lat: '78.198408',
     lon: '48.5106548',
     url:
         'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/node/56828/mariya-shevchenko-kardiogramma-gor-462677-beluha.jpg?itok=X2o_utMw',
-    details: 'details details details details',
+    details: 'details details',
     type: 'Ресторан',
   ),
   Sight(
@@ -26,7 +25,16 @@ final List<Sight> mocks = [
     lon: '48.5106548',
     url:
         'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/node/56828/mariya-shevchenko-kardiogramma-gor-462677-beluha.jpg?itok=X2o_utMw',
-    details: 'details details details details',
+    details: 'details details',
+    type: 'Музей',
+  ),
+  Sight(
+    name: 'Name 4',
+    lat: '78.198408',
+    lon: '48.5106548',
+    url:
+        'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/node/56828/mariya-shevchenko-kardiogramma-gor-462677-beluha.jpg?itok=X2o_utMw',
+    details: 'details details',
     type: 'Музей',
   ),
 ];
