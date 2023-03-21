2ca7f9ef131923b4fcce327e58e2bbf0bad89203
D-Dolovanov <da.golovanov@yandex.ru>
2023-03-21 22:45:43 +0300

    4-8-constraints

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -5,6 +5,58 @@ import 'package:places/assets/strings.dart';
 import 'package:places/mocks.dart';
 import 'package:places/ui/screen/sight_card.dart';
 
+class BigAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BigAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final statusBarHeigth = MediaQuery.of(context).padding.top;
+
+    final heightAppBar = context.findRenderObject() as RenderBox;
+    // ignore: avoid_print
+    print(heightAppBar.size.height - statusBarHeigth); //132.0
+
+    return Column(
+      // height: heigth,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: statusBarHeigth + 40.0),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            AppStrings.appTitle,
+            textAlign: TextAlign.left,
+            style: AppTypography.text32AppTitle,
+          ),
+        ),
+        const SizedBox(height: 16.0),
+      ],
+    );
+    // AppBar(
+    //   backgroundColor: AppColors.white,
+    //   elevation: 0.0,
+    //   toolbarHeight: 0.0,
+    //   bottom: const PreferredSize(
+    //     preferredSize: Size.fromHeight(72 + 40),
+    //     child: SizedBox(
+    //       width: double.infinity,
+    //       child: Padding(
+    //         padding: EdgeInsets.symmetric(horizontal: 16.0),
+    //         child: Text(
+    //           AppStrings.appTitle,
+    //           textAlign: TextAlign.left,
+    //           style: AppTypography.text32AppTitle,
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(132.0);
+}
+
 class SightListScreen extends StatefulWidget {
   const SightListScreen({super.key});
 
@@ -18,28 +70,10 @@ class _SightListScreenState extends State<SightListScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.white,
-      appBar: AppBar(
-        backgroundColor: AppColors.white,
-        elevation: 0.0,
-        toolbarHeight: 0.0,
-        bottom: const PreferredSize(
-          preferredSize: Size.fromHeight(72.0 + 40.0),
-          child: SizedBox(
-            width: double.infinity,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                AppStrings.appTitle,
-                textAlign: TextAlign.left,
-                style: AppTypography.text32AppTitle,
-              ),
-            ),
-          ),
-        ),
-      ),
+      appBar: const BigAppBar(),
       body: Column(
         children: [
-          const SizedBox(height: 16.0),
+          // const SizedBox(height: 16.0),
           Expanded(
             child: ListView.separated(
               padding: const EdgeInsets.symmetric(horizontal: 16.0),
